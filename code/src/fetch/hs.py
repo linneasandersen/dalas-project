dc270ec2df32fbc26f6c7b598680c1dbf6f22345
linneasandersen <linneasandersen@gmail.com>
2025-11-23 16:20:26 +0100

    path

--- code/src/fetch/hs.py
+++ code/src/fetch/hs.py
@@ -3,8 +3,9 @@ from src.config import RAW_DIR, RAW_DIR_EN
 
 def fetch_hs():
     # check which directory to use
-    if RAW_DIR_EN.exists():
-        df = pd.read_csv(RAW_DIR_EN / "hs_product_classification.csv")
+    path = RAW_DIR_EN / "hs_product_classification.csv"
+    if path.exists():
+        df = pd.read_csv(path)
     else:
         df = pd.read_csv(RAW_DIR / "hs_product_classification.csv")
     return df

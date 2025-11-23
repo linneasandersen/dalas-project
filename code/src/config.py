c5f1c0997347f1a8db0a2795858428c2ee54649a
linneasandersen <linneasandersen@gmail.com>
2025-11-23 16:17:53 +0100

    changed path

--- code/src/config.py
+++ code/src/config.py
@@ -1,7 +1,12 @@
 from pathlib import Path
 
 GOOGLE_DRIVE = Path("/Users/linneaandersen/Google Drive/Mit drev/DALAS/data")
+GOOGLE_DRIVE_EN = Path("/Users/linneaandersen/Google Drive/My Drive/DALAS/data")
+
 RAW_DIR = GOOGLE_DRIVE / "raw"
 PROCESSED_DIR = GOOGLE_DRIVE / "processed"
+
+RAW_DIR_EN = GOOGLE_DRIVE_EN / "raw"
+PROCESSED_DIR_EN = GOOGLE_DRIVE_EN / "processed"
 
 OEC_YEARS = range(2022, 2023)
--- code/src/fetch/hs.py
+++ code/src/fetch/hs.py
@@ -1,6 +1,10 @@
 import pandas as pd
-from src.config import PROCESSED_DIR, RAW_DIR
+from src.config import RAW_DIR, RAW_DIR_EN
 
 def fetch_hs():
-    df = pd.read_csv(RAW_DIR / "hs_product_classification.csv")
+    # check which directory to use
+    if RAW_DIR_EN.exists():
+        df = pd.read_csv(RAW_DIR_EN / "hs_product_classification.csv")
+    else:
+        df = pd.read_csv(RAW_DIR / "hs_product_classification.csv")
     return df
